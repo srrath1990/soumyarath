828d916172cf0ac3621f728df1328c6a0f91c28f
srrath1990 <soumya.rath99@gmail.com>
2025-05-04 23:27:31 +0530

    ok

---
+++ operator.py
@@ -0,0 +1,52 @@
+# Arithmetic operator
+#addition -- Display the sum of 3 and 8 ?
+x=3
+y=8
+print(x+y)
+
+#substraction -- Display the difference of 3 and 8?
+a=3
+b=8
+print(a-b)
+
+#product --  Display the product of 3 and 8?
+x=3
+y=8
+print(x*y)
+
+#Division -- Display the result of 8 divided by 3?
+p=8
+q=3
+print(p/q)
+
+#Modulus-- Display the remainder or modulus of 8 by 3?
+a=8
+b=3
+print(a%b)
+
+#exponention --  Display the result of 8 to the power of 3?
+a=8
+b=3
+print(a**b)
+
+# floor division --  Display the divisor of 8 by 3 ?
+a=8
+b=3
+print(a//b)
+
+#Comparison Operators
+
+# Equals?
+c = 5
+d = 5.0
+print(c == d)
+
+#Grater than
+a=5
+b=2
+print(5>2)
+
+#loweer than
+a=5
+b=2
+print(a<b)
